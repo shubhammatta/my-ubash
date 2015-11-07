8de655c3e258126def2d4d5eec305f8e3513dd3e
= <=>
2015-11-07 14:58:26 -0200

    Eclipse mars install

---
+++ install/eclipserun.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Eclipse Mars run
+
+# @FILESOURCE eclipserun.sh
+# @AUTHOR lucas andrey
+# @VERSION 1.0.0
+
+DESC='--- Eclipse mars running ---'
+
+# __BEGIN_MAIN__
+cd ~/eclipse/jee-mars/eclipse
+
+./eclipse
+
+echo $DESC
+# __END_MAIN__
