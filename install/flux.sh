2c900f1a02f88c0e93149ca6d656156a8483422c
arijitAD <arijitad.in@gmail.com>
2016-10-27 00:13:02 +0530

    Added Messages

--- install/flux.sh
+++ install/flux.sh
@@ -6,6 +6,8 @@ DESC='--- f.lux successfully installed ---'
 
 # __BEGIN_MAIN__
 sudo add-apt-repository ppa:nathan-renniewaldock/flux
++echo '--- f.lux repository added ---'
 sudo apt-get update
 sudo apt-get install fluxgui
+echo $DESC
 # __END_MAIN__
--- install/vlc.sh
+++ install/vlc.sh
@@ -7,4 +7,5 @@ DESC='--- VLC successfully installed ---'
 # __BEGIN_MAIN__
 sudo apt-get update
 sudo apt-get install vlc
+echo $DESC
 # __END_MAIN__
